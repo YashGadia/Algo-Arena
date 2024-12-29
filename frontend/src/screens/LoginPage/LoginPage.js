e91e87d076c129f6ac673995d12d960315d581e4
YashGadia <101691073+YashGadia@users.noreply.github.com>
2024-12-29 16:38:06 +0530

    Created basic frontend of Login Page

--- frontend/src/screens/LoginPage/LoginPage.js
+++ frontend/src/screens/LoginPage/LoginPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import LoginPageImage from '../../images/websiteImages/LoginPageImage.png';
+import Button from '@material-ui/core/Button';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 import './LoginPage.css';
 
 const LoginPage = () => {
@@ -16,6 +18,11 @@ const LoginPage = () => {
           <div className="rightForm_Textfield">
             <TextField id="outlined-basic" label="Email" size="small" variant="outlined" style={{ width: 350 }} />
             <TextField id="outlined-basic" label="Pasword" size="small" variant="outlined" style={{ width: 350 }} />
+          </div>
+          <div className="rightForm_Button">
+          <Button variant="contained" style={{ backgroundColor: 'rgb(5, 151, 153)', color: 'white' }} startIcon={<LockOpenIcon />}>
+            Sign in
+          </Button>
           </div>
         </div>
       </div>
