ec032c1102fc7cbabc9925065d5ddf96929ffbe4
YashGadia <101691073+YashGadia@users.noreply.github.com>
2025-01-07 13:07:20 +0530

    Updated store.js to take the initial state from local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,31 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension"
-import { userLoginReducer } from "./reducers/userReducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
-    userLogin: userLoginReducer
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
